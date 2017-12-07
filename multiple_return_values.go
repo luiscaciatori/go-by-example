b05f99cc096e551e2c27f4c197ec4deb80bc0ed5
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2017-12-07 05:29:12 -0200

    Adde more functions examples

---
+++ multiple_return_values.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func sum(a, b int) int {
+    return a + b
+}
+
+func vals() (int, int) {
+    return 3, 7
+}
+
+func main() {
+
+    a, b := vals()
+    fmt.Println(a)
+    fmt.Println(b)
+
+    _, c := vals()
+    fmt.Println(c)
+
+    res := sum(vals())
+    fmt.Println(res)
+
+}
